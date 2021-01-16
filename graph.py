4c956d15d2d826b09a88c9f362c00ce443d7003f
denizat <deniza.telci@gmail.com>
2021-01-16 11:05:59 -0600

    Fixed some things

--- graph.py
+++ graph.py
@@ -16,12 +16,13 @@ TODO:
 import math as m, turtle as t
 t.bgcolor("black")
 t.color("white") 
-t.speed(5)
+t.speed(0)
+t.width(1)
 
 
 x = [[0,0]] # Array initialization and starting point 
-n = 10 # Number of sides
-r = 200 # Side length
+n = 25 # Number of sides
+r = 100 # Side length
 angle = 2*m.pi/n
 angleinc = angle
 def initalizePolygon():
@@ -36,43 +37,61 @@ def initalizePolygon():
 
 # Draws all of the connections between all of the points in the x array.
 def draw():
+    c = r/(2*m.sin(m.pi/n))
+    t.penup()
+    t.goto(0+r,0-c)
+    t.pendown()
     for i in range(len(x)):
         for y in range(i,len(x)):
-            t.goto(x[y])
-            t.goto(x[i])
-
-
-def checkY(a1,a2,b1,b2):
+            t.goto(x[y][0]+r,x[y][1]-c)
+            t.goto(x[i][0]+r,x[i][1]-c)
+            # What does including this do? t.goto(x[y][0]+r,x[y][1]-c)
 
 def between(a1,a2,b1,b2):
-def checkX(a1x,a2x,b1x,b2x):
-    if(between(a1x,b1x,a2x)
-# Takes line segments a1-a2 and b1-b2 and checks if the intersect.
+    if(a1 < b1 < a2 or a2 < b1 < a1):
+        return True
+    if(a1 < b2 < a2 or a2 < b2 < a1):
+        return True
+    if(b1 < a1 < b2 or b2 < a1 < b1):
+        return True
+    if(b1 < a2 < b2 or b2 < a2 < b1):
+        return True
+    return False
+
+
 def intersects(a1,a2,b1,b2):
-
-
+    return(between(a1[0],a2[0],b1[0],b2[0]) and between(a1[1],a2[1],b1[1],b2[1])) 
 '''
-def intersects():
-    return # True or false
-def intersection():
-    return [a,b]
+# Completely Broken
 
-while(epochs > 0):
-    epochs -= 1
-    if epochs > 1:
-        length = len(x) 
-        for i in range(length):
-            for j in range(i,length):
-                for h in range(length):
-                    if h = i or h = j:
-                        h += 1
-                    for k in range(h,length):
-                        if k = i or k = j:
-                            k += 1
-                        if(intersects(x[i],x[j])):
-                            x.append(intersection(x[i],x[j]))
+def intersectionPoint(a1,a2,b1,b2):
+    # Division by zero shouldnt be happening but it is so this is here
+    if(a1[0]== a2[0] or b1[0] == b2[0]):
+        return [0,0]
+    ma = (a1[1]-a2[1])/(a1[0]-a2[0]) 
+    ba = a1[1]-ma*a1[0]
+    mb = (b1[1]-b2[1])/(b1[0]-b2[0])
+    bb = b1[1]-mb*b1[0]
+    if(ma == mb):
+        return [0,0]
+    x = (bb-ba)/(ma-mb)
+    y = (bb-ba)/(mb-ma)
+    return [x,y]
+
+def makeIntersection(a1,a2,b1,b2):
+    if(intersects(a1,a2,b1,b2)):
+            x.append(intersectionPoint(a1,a2,b1,b2))
+
+def iterate():
+    length = len(x)
+    for i in range(length):
+        for j in range(length):
+            for h in range(length):
+                for k in range(length):
+                    makeIntersection(x[i],x[j],x[h],x[k])
 '''
 
 initalizePolygon()
+# iterate()
 draw()
-done()
+t.done()
